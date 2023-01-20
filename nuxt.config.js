d7ad8301dc966bc25f2f07cb909f4b43a518985b
xan-98 <samsung531253125312@gmail.com>
2023-01-20 20:41:38 +0500

    tls 01

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,6 +28,7 @@ export default {
   plugins: [
     '~/plugins/vue-agile.js',
     '~/plugins/tr.js',
+    '~/plugins/axios.js'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
---
+++ plugins/axios.js
@@ -0,0 +1,14 @@
+import https from 'https';
+
+export default function ({
+  $axios
+}) {
+  const agent = new https.Agent({
+    rejectUnauthorized: false
+  });
+  $axios.onRequest(config => {
+    config.httpsAgent = agent;
+    // if (process.env.dev) {
+    // }
+  });
+}
